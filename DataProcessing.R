94b921dcda57126830aae97f1dd6c1f1f2c83418
NatanelHa <43453956+NatanelHa@users.noreply.github.com>
2021-06-29 14:53:37 -0400

    Adjusting collar_map and printing Summary
    In order to group by treatment, I think it might be better to leave collar_map in wide form, but still convert fluxes to long form. Also, I have calculated and printed a summary after grouping by treatment.

--- DataProcessing.R
+++ DataProcessing.R
@@ -7,29 +7,32 @@ library(ggplot2)
 collar_map <- readr::read_csv("collar_map.csv")
 fluxes <- readr::read_csv("fluxes.csv")
 
-#Convert Group to Character
-collar_map$Group <- as.character(collar_map$Group)
+#Clean up collar_map
+collar_map %>%
+  select(-LI8100A_Port)->
+  collar_map
 
 #Wide to Long Data
-collar_map %>%
-  select(-LI8100A_Port) %>%
-  pivot_longer(-Collar, names_to = "Variable") ->
-  collar_map_long
-
 fluxes %>%
   pivot_longer(-Collar, names_to="Year") ->
   fluxes_long
 
 #Filter Out NA values
-collar_map_long %>%
-  filter(!is.na(value))->
-  collar_map_long
-
 fluxes_long %>%
   filter(!is.na(value))->
   fluxes_long
 
 #Merge the two tables
 fluxes_long %>%
-  left_join(collar_map_long, by="Collar")->
+  left_join(collar_map, by="Collar")->
   fluxes_merged
+
+#Compute and Print Summary
+fluxes_merged %>%
+  group_by(Treatment) %>%
+  summarise(mean = mean(value), N = n(), Std_dev = sd(value))->
+  summary_treatment
+
+summary_treatment %>%
+  print()
+
