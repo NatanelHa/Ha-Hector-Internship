f40a2590316246a410c650870f646dd8aca0cbdb
NatanelHa <43453956+NatanelHa@users.noreply.github.com>
2021-06-29 14:14:04 -0400

    Importing libraries and Converting to Long
    Importing the libraries from the 'tidyverse'. Also, converting collar_map and fluxes to long table form.

--- DataProcessing.R
+++ DataProcessing.R
@@ -1,2 +1,23 @@
+#Importing Libraries
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+
+#Reading Datasets
 collar_map <- readr::read_csv("collar_map.csv")
 fluxes <- readr::read_csv("fluxes.csv")
+
+#Convert Group to Character
+collar_map$Group <- as.character(collar_map$Group)
+
+#Wide to Long Data
+collar_map %>%
+  select(-LI8100A_Port) %>%
+  pivot_longer(-Collar, names_to = "Variable") ->
+  collar_map_long
+
+fluxes %>%
+  pivot_longer(-Collar, names_to="Year")->
+  fluxes_long
+
+
