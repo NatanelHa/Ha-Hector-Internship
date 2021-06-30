4f1bba820c1b21d50d5691a9ea5a9e52d293f12f
NatanelHa <43453956+NatanelHa@users.noreply.github.com>
2021-06-30 13:27:09 -0400

    Condensing code
    There were several sections of code that was combined together to be shorter.

--- DataProcessing.R
+++ DataProcessing.R
@@ -7,33 +7,22 @@ library(ggplot2)
 collar_map <- readr::read_csv("collar_map.csv")
 fluxes <- readr::read_csv("fluxes.csv")
 
-#Clean up collar_map
+#Select treatment and collar from collar_map
 collar_map %>%
   select(Treatment, Collar)->
   treatment 
 
-#Wide to Long Data
+#Creating a long data table from fluxes and collar_map
 fluxes %>%
-  pivot_longer(-Collar, names_to="Date") ->
-  fluxes_long
-
-#Filter Out NA values
-fluxes_long %>%
-  filter(!is.na(value))->
-  fluxes_long
-
-#Merge the two tables
-fluxes_long %>%
-  left_join(treatment, by="Collar")->
+  pivot_longer(-Collar, names_to="Date") %>% #Wide to Long
+  filter(!is.na(value))%>%                   #Filter out NA
+  left_join(treatment, by="Collar")->        #Merging 
   fluxes_merged
 
 #Compute and Print Summary
 fluxes_merged %>%
   group_by(Treatment) %>%
-  summarise(mean = mean(value), N = n(), Std_dev = sd(value))->
-  summary_treatment
-
-summary_treatment %>%
+  summarise(mean = mean(value), N = n(), Std_dev = sd(value))%>% 
   print()
 
 #Boxplot with overlaid dots
@@ -54,11 +43,10 @@ fluxes_merged %>%
                  width = 0.2)+
   theme_grey()
 
-#Finding out which collars do not appear in fluxes
+#Finding out which collars do not appear in fluxes and printing 
 collar_map %>%
-  anti_join(fluxes_long, by="Collar")
+  anti_join(fluxes, by="Collar")
 
-#Printing which collars do not appear in fluxes
 print("Collar 34 is listed in the metadata that do not appear in the fluxes data file.")
 print("Collar 34 is in the Fresh Plot, in Group 9, and the treatment was Disturbance")  
                  
