e72ca5b6f664d56d2c7cb86b25094dea226079b8
NatanelHa <43453956+NatanelHa@users.noreply.github.com>
2021-07-16 16:22:21 -0400

    Running Core85 and getting results
    Running core for scenario of rcp 8.5 and getting results.

--- HectorAssignment.R
+++ HectorAssignment.R
@@ -6,3 +6,13 @@ library(dplyr)
 #Creating rcp85 Core
 rcp85 <- system.file("input", "hector_rcp85.ini", package = "hector")
 core85 <- newcore(rcp85, suppresslogging  = TRUE)
+
+#Run Core
+run(core85)
+
+#Getting Results
+result_vars <- c(ATMOSPHERIC_C(), SOIL_C(), VEG_C(), DETRITUS_C(), OCEAN_C())
+results85 <- fetchvars(core85, 2000:2100, result_vars, scenario = "RCP 8.5")
+
+
+
