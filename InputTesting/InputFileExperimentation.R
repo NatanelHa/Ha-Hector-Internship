a499cda9ef01246fb1aafa787b99244b5a9033ff
NatanelHa <43453956+NatanelHa@users.noreply.github.com>
2021-07-26 16:34:10 -0400

    Comparing a 3x and 7x pump to a normal run.
    Pumping carbon 3x and 7x in different emissions pathways for rcp45 in 2050, and comparing the results in several plots.

--- InputTesting/InputFileExperimentation.R
+++ InputTesting/InputFileExperimentation.R
@@ -4,150 +4,111 @@ library(ggplot2)
 library(dplyr)
 library(tidyr)
 
+#Set Theme
+theme_set(theme_bw())
+
 ## Basic Run
 #Configuring INI File
-ini_file <- "/Users/Natanel Ha/Documents/GitHub/Ha-Hector-Internship/InputTesting/myInput.ini"
+ini_file_norm <- system.file("input", "hector_rcp45.ini", package = "hector")
+ini_file_3pump <- "/Users/Natanel Ha/Documents/GitHub/Ha-Hector-Internship/InputTesting/myInputPump3.ini"
+ini_file_7pump <- "/Users/Natanel Ha/Documents/GitHub/Ha-Hector-Internship/InputTesting/myInputPump7.ini"
 
 #Initialize a Hector Instance
-core <- newcore(ini_file)
-core 
+coreNorm <- newcore(ini_file_norm)
+core3pump <- newcore(ini_file_3pump)
+core7pump <- newcore(ini_file_7pump)
 
-#Run the Core
-run(core) 
+#Run the Cores
+run(coreNorm) 
+run(core3pump)
+run(core7pump)
 
 #Retrieve Results
-results <- fetchvars(core, 2000:2500)
-head(results) 
+resultsNorm <- fetchvars(coreNorm, 2000:2100, scenario = "Normal")
+results3 <- fetchvars(core3pump, 2000:2100, scenario = "Pump 3x")
+results7 <- fetchvars(core7pump, 2000:2100, scenario = "Pump 7x")
+
+#Retrieve fluxes Results 
+result_vars <- c(OCEAN_CFLUX(), LAND_CFLUX(), FFI_EMISSIONS())
+resultsNormFlux <- fetchvars(coreNorm, 2000:2100, result_vars, scenario = "Normal")
+results3Flux <- fetchvars(core3pump, 2000:2100, result_vars,  scenario = "Pump 3x")
+results7Flux <- fetchvars(core7pump, 2000:2100, result_vars,  scenario = "Pump 7x")
+
+##Calculating atmosphere flux
+resultsNormFlux %>%
+  select(-units) %>%
+  pivot_wider(names_from = variable) %>%
+  mutate(atmosphere_flux = (ffi_emissions - atm_ocean_flux - atm_land_flux))%>%
+  select(-ffi_emissions) %>%
+  pivot_longer(3:5, names_to = "variable")->
+  resultsNormFlux
+
+results3Flux %>%
+  select(-units) %>%
+  pivot_wider(names_from = variable) %>%
+  mutate(atmosphere_flux = (ffi_emissions - atm_ocean_flux - atm_land_flux))%>%
+  select(-ffi_emissions) %>%
+  pivot_longer(3:5, names_to = "variable")->
+  results3Flux
+
+results7Flux %>%
+  select(-units) %>%
+  pivot_wider(names_from = variable) %>%
+  mutate(atmosphere_flux = (ffi_emissions - atm_ocean_flux - atm_land_flux))%>%
+  select(-ffi_emissions) %>%
+  pivot_longer(3:5, names_to = "variable")->
+  results7Flux
 
 
-#Plotting Results
-ggplot(results) +
-  aes(x = year, y = value) +
+#Combining
+results <- rbind(resultsNorm, results3, results7)
+resultsFlux <- rbind(resultsNormFlux, results3Flux, results7Flux)
+
+#Line graph
+##Line Graph
+lineGraphCompare <- ggplot(results)+
+  aes(x = year, y = value, color = scenario) +
   geom_line() +
   facet_wrap(~variable, scales = "free_y", 
              labeller = labeller(variable = c("Ca" = "CO2 Concentration (ppmv CO2)",
                                               "Ftot" = "Total Radiative Forcing (W/m2)",
                                               "FCO2" = "CO2 Forcing (W/m2)",
                                               "Tgav" = "Global Mean Temperature (degrees C)")))+
-  ylab(NULL)
-
-#Getting Results
-result_vars <- c(ATMOSPHERIC_C(), SOIL_C(), VEG_C(), DETRITUS_C(), OCEAN_C())
-results <- fetchvars(core, 2000:2100, result_vars, scenario = "RCP 4.5")
-
-#Plotting results
-##Line Graph
-lineGraph <- ggplot(results)+
-  aes(x = year, y = value) +
-  geom_line() +
-  facet_wrap(~variable, scales = "free_y", 
-             labeller = labeller(variable = c("atmos_c" = "Atmospheric",
-                                              "veg_c" = "Vegetation",
-                                              "soil_c" = "Soil",
-                                              "detritus_c" = "Detritus",
-                                              "ocean_c" = "Ocean")))+
-  ylab("Carbon (Pg C)")+
-  xlab("Year")+
-  theme_bw()
-
-##Area Graph
-areaGraph <- ggplot(results)+
-  aes(x = year, y = value, group = scenario, fill = variable)+
-  geom_bar(stat = "identity")+
-  ylab("Carbon (Pg C)") +
-  scale_fill_discrete(labels = c("Atmosphere", "Detritus","Ocean", "Soil", "Vegetation"))+
-  geom_col(width = 1)+
-  guides(fill = guide_legend(title = "Carbon Pools"))+
-  xlab("Year")+
-  theme_bw()
-
-##Bar Graph Filtering
-results %>%
-  filter(year %% 100 == 0)->
-  resultsCentury
-
-#Collected in One Bar
-barPlot <- ggplot(resultsCentury)+
-  aes(x = as.character(year), y = value, fill = variable)+
-  geom_bar(stat = "identity")+
-  ylab("Carbon (Pg C)") +
-  scale_fill_discrete(labels = c("Atmosphere", "Detritus","Ocean", "Soil", "Vegetation"))+
-  guides(fill = guide_legend(title = "Carbon Pools"))+
-  xlab("Year")+
-  theme_bw()
-
-#Split into separate Facets
-splitBarPlot <- ggplot(resultsCentury)+
-  aes(x = as.character(year), y = value)+
-  geom_bar(stat = "identity")+
-  facet_wrap(~variable, scales = "free_y", 
-             labeller = labeller(variable = c("atmos_c" = "Atmospheric",
-                                              "veg_c" = "Vegetation",
-                                              "soil_c" = "Soil",
-                                              "detritus_c" = "Detritus",
-                                              "ocean_c" = "Ocean")))+
-  ylab("Carbon (Pg C)") +
-  xlab("Year")+
-  theme_bw()
-
-##Looking at fluxes
-result_vars <- c(OCEAN_CFLUX(), LAND_CFLUX(), FFI_EMISSIONS())
-resultsflux <- fetchvars(core, 2000:2100, result_vars, scenario = "RCP 4.5")
-
-#Calculating atmosphere flux
-resultsflux %>%
-  select(-units) %>%
-  pivot_wider(names_from = variable) %>%
-  mutate(atmosphere_flux = (ffi_emissions - atm_ocean_flux - atm_land_flux))%>%
-  select(-ffi_emissions) %>%
-  pivot_longer(3:5, names_to = "variable")->
-  resultsflux
+  ylab(NULL)+
+  guides(color = guide_legend(title = "Scenario"))+
+  xlab("Year")
+  
 
 ##Plotting Fluxes
-#Not Faceted
-fluxLine <- ggplot(resultsflux)+
-  aes(x = year, y = value, color = variable) +
-  geom_line() +
-  scale_color_discrete(labels = c("Land", "Ocean", "Atmosphere"))+
-  guides(color = guide_legend(title = "Carbon Flux(Pg C/yr)"))+
-  ylab("Carbon Flux(Pg C/yr)")+
-  xlab("Year")+
-  theme_bw()
-
 #Faceted
-fluxFacetLine <- ggplot(resultsflux)+
-  aes(x = year, y = value) +
+fluxFacetLineCompare <- ggplot(resultsFlux)+
+  aes(x = year, y = value, color = scenario) +
   geom_line() +
   facet_wrap(~variable, scales = "free_y",
              labeller = labeller(variable = c("atm_land_flux" = "Land Net Flux",
                                               "atm_ocean_flux" = "Ocean Net Flux",
                                               "atmosphere_flux" = "Atmosphere Net Flux")))+
   ylab("Carbon Flux(Pg C/yr)")+
-  xlab("Year")+
-  theme_bw()
+  xlab("Year")
+  
 
 ##Bar Graph Fluxes
-resultsflux %>%
+resultsFlux %>%
   filter(year %% 100 == 0)->
   resultsfluxCentury
 
-fluxBar <- ggplot(resultsfluxCentury)+
-  aes(x = as.character(year), y = value)+
-  geom_bar(stat = "identity")+
+fluxBarCompare <- ggplot(resultsfluxCentury)+
+  aes(x = as.character(year), y = value, fill = scenario)+
+  geom_bar(stat = "identity", position = "dodge")+
   facet_wrap(~variable, 
              labeller = labeller(variable = c("atm_land_flux" = "Land Net Flux",
                                               "atm_ocean_flux" = "Ocean Net Flux",
                                               "atmosphere_flux" = "Atmosphere Net Flux")))+
   ylab("Carbon Flux(Pg C/yr)") +
-  xlab("Year")+
-  theme_bw()
+  xlab("Year")
 
-#RCP 4.5 Plots
-lineGraph
-areaGraph
-barPlot
-splitBarPlot
-fluxLine
-fluxFacetLine
-fluxBar 
 
+#Comparing RCPs
+lineGraphCompare
+fluxFacetLineCompare
+fluxBarCompare
