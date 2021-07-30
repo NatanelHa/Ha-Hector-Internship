26bbd37e8d03dea3ac83f7e2277d9e1e2caef49f
NatanelHa <43453956+NatanelHa@users.noreply.github.com>
2021-07-30 09:42:39 -0400

    Tracking RCP 2.6 and RCP 4.5

--- New Scenarios/TrackingTesting.R
+++ New Scenarios/TrackingTesting.R
@@ -1,20 +1,29 @@
 #Importing libraries
 library(hector)
 library(ggplot2)
-library(latex2exp)
 
 ## Basic Run
-#Configuring INI File
-ini_file <- system.file("input/hector_rcp26.ini", package = "hector") 
+#Configuring INI Files
+ini_file26 <- system.file("input/hector_rcp26.ini", package = "hector") 
+ini_file45 <- system.file("input/hector_rcp45.ini", package = "hector") 
 
-#Initialize a Hector Instance
-core <- newcore(ini_file)
-core 
+#Initialize the Cores
+core26 <- newcore(ini_file26)
+core45 <- newcore(ini_file45)
 
 #Run the Core
-run(core) 
+run(core26) 
+run(core45) 
 
-td <- get_tracking_data(core)
-tracking <-read.csv(textConnection(td))
+#Getting tracking
+td26 <- get_tracking_data(core26)
+td45 <- get_tracking_data(core45)
 
-shutdown(core)
+#Shutdown
+shutdown(core26)
+shutdown(core45)
+
+
+
+
+
