ff17783540294bf632255400fe229b7238f567a8
NatanelHa <43453956+NatanelHa@users.noreply.github.com>
2021-07-30 16:20:33 -0400

    Adding a plot based on amount
    Adding a plot based on amount of carbon instead of fraction.

--- New Scenarios/TrackingTesting.R
+++ New Scenarios/TrackingTesting.R
@@ -1,9 +1,10 @@
 #Importing libraries
 library(hector)
 library(ggplot2)
+library(dplyr)
 
 #tracking plot function
-tracking_plot <-function(ini_file, title) {
+tracking_plot <-function(ini_file, graph_type, title) {
   #establish core
   core <- newcore(ini_file)
   
@@ -16,30 +17,60 @@ tracking_plot <-function(ini_file, title) {
   #Filter out diff
   td <-
     results_with_diff %>%
-    filter(pool_name != "Diff")
+    filter(pool_name != "Diff") %>%
+    mutate(source_amount = source_fraction*pool_value)
   
   #Set Theme
   theme_set(theme_minimal())
   
-  areaGraph <- ggplot(td)+
-    aes(x = year, y = source_fraction, fill = source_name)+
-    geom_bar(stat = "identity")+
-    facet_wrap(~pool_name)+
-    geom_col(width = 1)+
-    guides(fill = guide_legend(title = "Carbon Pools"))+
-    ylab("Source Fraction") +
-    ggtitle(title)+
-    xlab("Year")
+  if (graph_type == "fraction"){
+    areaGraph <- ggplot(td)+
+      aes(x = year, y = source_fraction, fill = source_name)+
+      geom_bar(stat = "identity")+
+      facet_wrap(~pool_name, scales = "free_y")+
+      geom_col(width = 1)+
+      guides(fill = guide_legend(title = "Carbon Pools"))+
+      ylab("Source Fraction") +
+      ggtitle(title)+
+      xlab("Year")
+  }
+  else {
+    areaGraph<- ggplot(td)+
+      aes(x = year, y = source_amount, fill = source_name)+
+      geom_bar(stat = "identity")+
+      facet_wrap(~pool_name, scales = "free_y")+
+      geom_col(width = 1)+
+      guides(fill = guide_legend(title = "Carbon Pools"))+
+      ylab("Source Amount (Pg C)") +
+      ggtitle(title)+
+      xlab("Year")
+  }
   
+  #Shutdown Core
   shutdown(core)
   
   areaGraph
 }
 
-#Getting plots for each rcp
-tracking_plot(system.file("input/hector_rcp26.ini", package = "hector"), "RCP 2.6")
-tracking_plot(system.file("input/hector_rcp45.ini", package = "hector"), "RCP 4.5") 
-tracking_plot(system.file("input/hector_rcp60.ini", package = "hector"), "RCP 6.0")
-tracking_plot(system.file("input/hector_rcp85.ini", package = "hector"), "RCP 8.5")
+#Getting plots for each RCP
+tracking_plot(system.file("input/hector_rcp26.ini", package = "hector"),
+              "fraction", "RCP 2.6")
+tracking_plot(system.file("input/hector_rcp26.ini", package = "hector"),
+              "amount", "RCP 2.6")
+
+tracking_plot(system.file("input/hector_rcp45.ini", package = "hector"), 
+              "fraction", "RCP 4.5") 
+tracking_plot(system.file("input/hector_rcp45.ini", package = "hector"), 
+              "amount", "RCP 4.5") 
+
+tracking_plot(system.file("input/hector_rcp60.ini", package = "hector"), 
+              "fraction", "RCP 6.0")
+tracking_plot(system.file("input/hector_rcp60.ini", package = "hector"), 
+              "amount", "RCP 6.0")
+
+tracking_plot(system.file("input/hector_rcp85.ini", package = "hector"), 
+              "fraction", "RCP 8.5")
+tracking_plot(system.file("input/hector_rcp85.ini", package = "hector"), 
+              "amount", "RCP 8.5")
 
 
