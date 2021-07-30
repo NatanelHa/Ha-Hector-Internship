fec1fa6a7865d3545d816d9ca961e4295fd941df
NatanelHa <43453956+NatanelHa@users.noreply.github.com>
2021-07-30 10:08:08 -0400

    Area graphs for tracking
    Recreating the area graphs!!

--- New Scenarios/TrackingTesting.R
+++ New Scenarios/TrackingTesting.R
@@ -16,8 +16,41 @@ run(core26)
 run(core45) 
 
 #Getting tracking
-td26 <- get_tracking_data(core26)
-td45 <- get_tracking_data(core45)
+td26_with_diff <- get_tracking_data(core26)
+td45_with_diff <- get_tracking_data(core45)
+
+#Filter out diff
+td26 <-
+  td26_with_diff %>%
+  filter(pool_name != "Diff")
+
+td45<-
+  td45_with_diff %>%
+  filter(pool_name != "Diff")
+
+#Set Theme
+theme_set(theme_minimal())
+
+##Area Graphs
+areaGraph26 <- ggplot(td26)+
+  aes(x = year, y = source_fraction, fill = source_name)+
+  geom_bar(stat = "identity")+
+  facet_wrap(~pool_name)+
+  geom_col(width = 1)+
+  guides(fill = guide_legend(title = "Carbon Pools"))+
+  ylab("Source Fraction") +
+  xlab("Year")
+areaGraph26
+
+areaGraph45 <- ggplot(td45)+
+  aes(x = year, y = source_fraction, fill = source_name)+
+  geom_bar(stat = "identity")+
+  facet_wrap(~pool_name)+
+  geom_col(width = 1)+
+  guides(fill = guide_legend(title = "Carbon Pools"))+
+  ylab("Source Fraction") +
+  xlab("Year")
+areaGraph45
 
 #Shutdown
 shutdown(core26)
