d5e4d5247b523ee18e5591a8b7e51f14506fbfb4
NatanelHa <43453956+NatanelHa@users.noreply.github.com>
2021-08-02 15:19:28 -0400

    Fixing Formatting and adding comments
    Styling the code to look more clean and adding better commentation

--- Tracking/TrackingEarth.R
+++ Tracking/TrackingEarth.R
@@ -1,27 +1,30 @@
-#Importing libraries
+# Importing libraries
 library(hector)
 library(ggplot2)
 library(dplyr)
 
-#establish core
+# Establish core
 rcp26 <- system.file("input/hector_rcp26.ini", package = "hector")
-core <- newcore(rcp26, suppresslogging  = TRUE)
+core <- newcore(rcp26, suppresslogging = TRUE)
 
-#run core
+# Run core
 run(core)
 
-#Get Results
+# Get Results
 results <- get_tracking_data(core)
 
+# Calculating source anount
 results %>%
   filter(pool_name == "earth_c") %>%
   filter(year >= 2073) %>%
-  mutate(source_amount = source_fraction*pool_value)->
-  results
+  mutate(source_amount = source_fraction * pool_value) ->
+results
 
-earth_2073 <- 
+# Finding earth_c values in 2073
+# The year negative emissions starts
+earth_2073 <-
   results %>%
-  filter(source_name == "earth_c") 
+  filter(source_name == "earth_c")
 
 earth_2073_carbon <-
   first(earth_2073$source_amount)
@@ -29,45 +32,52 @@ earth_2073_carbon <-
 earth_2073_fraction <-
   first(earth_2073$source_fraction)
 
+# Calculating difference from 2073
 results %>%
-  mutate(source_amount = source_amount - earth_2073_carbon*(source_name=="earth_c"))%>%
-  mutate(source_fraction = source_fraction - earth_2073_fraction*(source_name=="earth_c"))->
-  results
+  mutate(source_amount = source_amount - earth_2073_carbon * (source_name == "earth_c")) %>%
+  mutate(source_fraction = source_fraction - earth_2073_fraction * (source_name == "earth_c")) ->
+results
 
-areaGraph<- ggplot(results)+
-  aes(x = year, y = source_amount, fill = source_name)+
-  geom_bar(stat = "identity")+
-  scale_fill_discrete(labels = c("Detritus",
-                                 "Vegetation",
-                                 "Soil",
-                                 "Earth",
-                                 "Atmosphere",
-                                 "High Level Ocean",
-                                 "Intermediate Ocean",
-                                 "Low Level Ocean",
-                                 "Deep Ocean"))+
-  geom_col(width = 1)+
-  guides(fill = guide_legend(title = "Carbon Pools"))+
+# Plotting in area graph of amount
+areaGraph <- ggplot(results) +
+  aes(x = year, y = source_amount, fill = source_name) +
+  geom_bar(stat = "identity") +
+  scale_fill_discrete(labels = c(
+    "Detritus",
+    "Vegetation",
+    "Soil",
+    "Earth",
+    "Atmosphere",
+    "High Level Ocean",
+    "Intermediate Ocean",
+    "Low Level Ocean",
+    "Deep Ocean"
+  )) +
+  geom_col(width = 1) +
+  guides(fill = guide_legend(title = "Carbon Pools")) +
   ylab("Source Amount (Pg C)") +
-  ggtitle("Earth Pool (Net Change from 2073)")+
+  ggtitle("Earth Pool (Net Change from 2073)") +
   xlab("Year")
 areaGraph
 
-areaGraph2 <- ggplot(results)+
-  aes(x = year, y = source_fraction, fill = source_name)+
-  geom_bar(stat = "identity")+
-  scale_fill_discrete(labels = c("Detritus",
-                                 "Vegetation",
-                                 "Soil",
-                                 "Earth",
-                                 "Atmosphere",
-                                 "High Level Ocean",
-                                 "Intermediate Ocean",
-                                 "Low Level Ocean",
-                                 "Deep Ocean"))+
-  geom_col(width = 1)+
-  guides(fill = guide_legend(title = "Carbon Pools"))+
+# Plotting in area graph of fraction
+areaGraph2 <- ggplot(results) +
+  aes(x = year, y = source_fraction, fill = source_name) +
+  geom_bar(stat = "identity") +
+  scale_fill_discrete(labels = c(
+    "Detritus",
+    "Vegetation",
+    "Soil",
+    "Earth",
+    "Atmosphere",
+    "High Level Ocean",
+    "Intermediate Ocean",
+    "Low Level Ocean",
+    "Deep Ocean"
+  )) +
+  geom_col(width = 1) +
+  guides(fill = guide_legend(title = "Carbon Pools")) +
   ylab("Source Fraction") +
-  ggtitle("Earth Pool fraction (Net Change from 2073)")+
+  ggtitle("Earth Pool fraction (Net Change from 2073)") +
   xlab("Year")
 areaGraph2
--- Tracking/TrackingTesting.R
+++ Tracking/TrackingTesting.R
@@ -1,121 +1,136 @@
-#Importing libraries
+# Importing libraries
 library(hector)
 library(ggplot2)
 library(dplyr)
 
-#tracking plot function
+# tracking plot function
 tracking_plot <- function(ini_file, graph_type, title) {
-  #establish core
+  # establish core
   core <- newcore(ini_file)
-  
-  #run core
+
+  # run core
   run(core)
-  
-  #Get Results
+
+  # Get Results
   results_with_diff <- get_tracking_data(core)
   results_with_diff
-  
-  #Filter out diff
+
+  # Filter out diff
   td <-
     results_with_diff %>%
     filter(pool_name != "Diff") %>%
-    mutate(source_amount = source_fraction*pool_value)
-    
-  #Changing Order
-  td$pool_namef = factor(td$pool_name, levels=c("detritus_c_global",
-                                                "veg_c_global",
-                                                "soil_c_global",
-                                                "earth_c",
-                                                "atmos_c",
-                                                "HL",
-                                                "intermediate",
-                                                "LL",
-                                                "deep"))
-  
-  
-  #Set Theme
+    mutate(source_amount = source_fraction * pool_value)
+
+  # Changing Order
+  td$pool_namef <- factor(td$pool_name, levels = c(
+    "detritus_c_global",
+    "veg_c_global",
+    "soil_c_global",
+    "earth_c",
+    "atmos_c",
+    "HL",
+    "intermediate",
+    "LL",
+    "deep"
+  ))
+
+
+  # Set Theme
   theme_set(theme_minimal())
-  
-  if (graph_type == "fraction"){
-    areaGraph <- ggplot(td)+
-      aes(x = year, y = source_fraction, fill = source_name)+
-      geom_bar(stat = "identity")+
+
+  # Graph of Fractional Portion
+  if (graph_type == "fraction") {
+    areaGraph <- ggplot(td) +
+      aes(x = year, y = source_fraction, fill = source_name) +
+      geom_bar(stat = "identity") +
       facet_wrap(~pool_namef,
-                 labeller = labeller(pool_namef = c("detritus_c_global" = "Detritus",
-                                                "veg_c_global" = "Vegetation",
-                                                "soil_c_global" = "Soil",
-                                                "earth_c" = "Earth",
-                                                "atmos_c" = "Atmosphere",
-                                                "HL" = "High Level Ocean",
-                                                "intermediate" = "Intermediate Ocean",
-                                                "LL" = "Low Level Ocean",
-                                                "deep" = "Deep Ocean")))+
-      scale_fill_discrete(labels = c("Detritus",
-                                     "Vegetation",
-                                     "Soil",
-                                     "Earth",
-                                     "Atmosphere",
-                                     "High Level Ocean",
-                                     "Intermediate Ocean",
-                                     "Low Level Ocean",
-                                     "Deep Ocean"))+
-      geom_col(width = 1)+
-      guides(fill = guide_legend(title = "Carbon Pools"))+
+        labeller = labeller(pool_namef = c(
+          "detritus_c_global" = "Detritus",
+          "veg_c_global" = "Vegetation",
+          "soil_c_global" = "Soil",
+          "earth_c" = "Earth",
+          "atmos_c" = "Atmosphere",
+          "HL" = "High Level Ocean",
+          "intermediate" = "Intermediate Ocean",
+          "LL" = "Low Level Ocean",
+          "deep" = "Deep Ocean"
+        ))
+      ) +
+      scale_fill_discrete(labels = c(
+        "Detritus",
+        "Vegetation",
+        "Soil",
+        "Earth",
+        "Atmosphere",
+        "High Level Ocean",
+        "Intermediate Ocean",
+        "Low Level Ocean",
+        "Deep Ocean"
+      )) +
+      geom_col(width = 1) +
+      guides(fill = guide_legend(title = "Carbon Pools")) +
       ylab("Source Fraction") +
-      ggtitle(title)+
+      ggtitle(title) +
       xlab("Year")
-  }
-  else {
-    areaGraph<- ggplot(td)+
-      aes(x = year, y = source_amount, fill = source_name)+
-      geom_bar(stat = "identity")+
-      facet_wrap(~pool_namef, scales = "free_y", 
-                 labeller = labeller(pool_namef = c("detritus_c_global" = "Detritus",
-                                                  "veg_c_global" = "Vegetation",
-                                                  "soil_c_global"= "Soil",
-                                                  "earth_c" = "Earth",
-                                                  "atmos_c" = "Atmosphere",
-                                                  "HL" = "High Level Ocean",
-                                                  "intermediate" = "Intermediate Ocean",
-                                                  "LL" = "Low Level Ocean",
-                                                  "deep" = "Deep Ocean")))+
-      scale_fill_discrete(labels = c("Detritus",
-                                     "Vegetation",
-                                     "Soil",
-                                     "Earth",
-                                     "Atmosphere",
-                                     "High Level Ocean",
-                                     "Intermediate Ocean",
-                                     "Low Level Ocean",
-                                     "Deep Ocean"))+
-      geom_col(width = 1)+
-      guides(fill = guide_legend(title = "Carbon Pools"))+
+
+    # Graph of the amunt of Carbon
+  } else {
+    areaGraph <- ggplot(td) +
+      aes(x = year, y = source_amount, fill = source_name) +
+      geom_bar(stat = "identity") +
+      facet_wrap(~pool_namef,
+        scales = "free_y",
+        labeller = labeller(pool_namef = c(
+          "detritus_c_global" = "Detritus",
+          "veg_c_global" = "Vegetation",
+          "soil_c_global" = "Soil",
+          "earth_c" = "Earth",
+          "atmos_c" = "Atmosphere",
+          "HL" = "High Level Ocean",
+          "intermediate" = "Intermediate Ocean",
+          "LL" = "Low Level Ocean",
+          "deep" = "Deep Ocean"
+        ))
+      ) +
+      scale_fill_discrete(labels = c(
+        "Detritus",
+        "Vegetation",
+        "Soil",
+        "Earth",
+        "Atmosphere",
+        "High Level Ocean",
+        "Intermediate Ocean",
+        "Low Level Ocean",
+        "Deep Ocean"
+      )) +
+      geom_col(width = 1) +
+      guides(fill = guide_legend(title = "Carbon Pools")) +
       ylab("Source Amount (Pg C)") +
-      ggtitle(title)+
+      ggtitle(title) +
       xlab("Year")
   }
-  
-  #Shutdown Core
+
+  # Shutdown Core
   shutdown(core)
-  
+
   areaGraph
 }
 
-#Getting plots for each RCP
-tracking_plot(system.file("input/hector_rcp26.ini", package = "hector"),
-             "fraction", "RCP 2.6")
-tracking_plot(system.file("input/hector_rcp26.ini", package = "hector"),
-              "amount", "RCP 2.6")
+# Getting plots for RCP 2.6 and 4.5
+tracking_plot(
+  system.file("input/hector_rcp26.ini", package = "hector"),
+  "fraction", "RCP 2.6"
+)
+tracking_plot(
+  system.file("input/hector_rcp26.ini", package = "hector"),
+  "amount", "RCP 2.6"
+)
 
-tracking_plot(system.file("input/hector_rcp45.ini", package = "hector"), 
-              "fraction", "RCP 4.5") 
-tracking_plot(system.file("input/hector_rcp45.ini", package = "hector"), 
-             "amount", "RCP 4.5") 
-
-tracking_plot(system.file("input/hector_rcp26 _1750.ini", package = "hector"),
-              "amount", "RCP 2.6")
-tracking_plot(system.file("input/hector_rcp26 _1850.ini", package = "hector"),
-              "amount", "RCP 2.6")
-tracking_plot(system.file("input/hector_rcp26 _1950.ini", package = "hector"),
-              "amount", "RCP 2.6")
-
+tracking_plot(
+  system.file("input/hector_rcp45.ini", package = "hector"),
+  "fraction", "RCP 4.5"
+)
+tracking_plot(
+  system.file("input/hector_rcp45.ini", package = "hector"),
+  "amount", "RCP 4.5"
+)
