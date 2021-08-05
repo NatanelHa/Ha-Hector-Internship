f170279012467e92943ca4b02c5214ba553053e7
NatanelHa <43453956+NatanelHa@users.noreply.github.com>
2021-08-05 11:46:36 -0400

    Testing non-Daccs scenario and updating RMarkdon
    Retrieving data on a non Dacs scenario and then updating the RMarkdown.

--- New Scenarios/JayScenario.R
+++ New Scenarios/JayScenario.R
@@ -10,7 +10,7 @@ data <- readr::read_csv(paste(path,
   sep = ""
 ))
 # setting scenario
-scenario <- "SSP5-1p9-DACCS-3pctHR"
+scenario <- "SSP5-1p9-3pctHR"
 
 # Filtering data
 data %>%
@@ -35,19 +35,19 @@ data %>%
   complete(Variable, Years = 2005:2100) %>%
   pivot_wider(names_from = Variable) %>%
   rename(ffi_emissions_with_negative = "Emissions|CO2|Energy and Industrial Processes") %>%
-  rename(luc_emissions = "Emissions|CO2|AFOLU") %>%
-  rename(daccs_uptake = "Carbon Sequestration|Direct Air Capture") ->
+  rename(luc_emissions = "Emissions|CO2|AFOLU") -> #%>%
+  #rename(daccs_uptake = "Carbon Sequestration|Direct Air Capture") ->
 data
 
 data$ffi_emissions_with_negative <- na.approx(data$ffi_emissions_with_negative, data$Years) / 3670
 
 data$luc_emissions <- na.approx(data$luc_emissions, data$Years) / 3670
 
-data$daccs_uptake <- na.approx(data$daccs_uptake, data$Years) / 3670
+#data$daccs_uptake <- na.approx(data$daccs_uptake, data$Years) / 3670
 
 data %>%
-  mutate(ffi_emissions = ffi_emissions_with_negative + daccs_uptake) %>%
-  select(Years, ffi_emissions, daccs_uptake, luc_emissions) ->
+  mutate(ffi_emissions = ffi_emissions_with_negative) %>% # + daccs_uptake) %>%
+  select(Years, ffi_emissions, luc_emissions) ->
 data
 
 write.csv(data, paste(path, "New Scenarios/", scenario, ".csv", sep = ""), row.names = FALSE)
