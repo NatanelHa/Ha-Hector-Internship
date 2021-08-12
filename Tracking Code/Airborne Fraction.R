bcac2b1ab08b42aa3b956d96340515062f82f483
NatanelHa <43453956+NatanelHa@users.noreply.github.com>
2021-08-12 11:25:43 -0400

    Improving Airborne Fraction Code
    Creating a function to make the Airborne Fraction easy to reproduce

--- Tracking Code/Airborne Fraction.R
+++ Tracking Code/Airborne Fraction.R
@@ -8,14 +8,14 @@ library(tidyr)
 tracking_results <- function(ini_file, start, stop, scenarioName) {
   # establish core
   core <- newcore(ini_file)
-  
+
   # run core
   run(core)
-  
+
   # Get Results
   results_with_diff <- get_tracking_data(core)
   results_with_diff
-  
+
   # Filter out diff and year
   td <-
     results_with_diff %>%
@@ -24,27 +24,33 @@ tracking_results <- function(ini_file, start, stop, scenarioName) {
     filter(year >= start) %>%
     mutate(source_amount = source_fraction * pool_value) %>%
     mutate(scenario = scenarioName)
-  
+
   return(td)
 }
 
-rcp26 <- system.file("input", "hector_rcp26.ini", package = "hector")
-td <- tracking_results(rcp26, 1900, 2000, "RCP 2.6")
+AF_calc <- function(ini_file, start, stop, scenarioName) {
+  td <- tracking_results(rcp26, 1900, 2050, "RCP 2.6")
 
-atm_c <- td %>%
-  filter(year==1900 | year==2000)%>%
-  filter(pool_name=="atmos_c")%>%
-  filter(source_name=="earth_c")
+  atm_c <- td %>%
+    filter(year == start | year == stop) %>%
+    filter(pool_name == "atmos_c") %>%
+    filter(source_name == "earth_c")
+
+
+  atm_c <- last(atm_c$source_amount) - first(atm_c$source_amount)
+
+  earth_c_loss <- td %>%
+    filter(year == start | year == stop) %>%
+    filter(pool_name == "earth_c") %>%
+    filter(source_name == "earth_c")
+
+  earth_c_loss <- first(earth_c_loss$source_amount) - last(earth_c_loss$source_amount)
+
+  AF <- atm_c / earth_c_loss
   
+  return(AF)
+}
 
-atm_c <- last(atm_c$source_amount) - first(atm_c$source_amount)
-
-earth_c_loss <- td %>%
-  filter(year==1900 | year==2000)%>%
-  filter(pool_name=="earth_c")%>%
-  filter(source_name=="earth_c")
-
-earth_c_loss <- first(earth_c_loss$source_amount)- last(earth_c_loss$source_amount)
-
-AF <- atm_c/earth_c_loss
+rcp26 <- system.file("input", "hector_rcp26.ini", package = "hector")
+AF_2000 <- AF_calc(rcp26, 1900, 2000, "RCP 2.6")
 
