46dc9358b07a07fe08f13708a40e58b7a7510695
NatanelHa <43453956+NatanelHa@users.noreply.github.com>
2021-08-12 13:00:11 -0400

    Comparing Airborne Fraction
    Comparing the Airborne Fraction in Hector to the one in the Knorr Paper

--- Tracking Code/Airborne Fraction.R
+++ Tracking Code/Airborne Fraction.R
@@ -28,9 +28,8 @@ tracking_results <- function(ini_file, start, stop, scenarioName) {
   return(td)
 }
 
-AF_calc <- function(ini_file, start, stop, scenarioName) {
-  td <- tracking_results(rcp26, 1900, 2050, "RCP 2.6")
-
+AF_calc <- function(td, start, stop) {
+  
   atm_c <- td %>%
     filter(year == start | year == stop) %>%
     filter(pool_name == "atmos_c") %>%
@@ -52,5 +51,29 @@ AF_calc <- function(ini_file, start, stop, scenarioName) {
 }
 
 rcp26 <- system.file("input", "hector_rcp26.ini", package = "hector")
-AF_2000 <- AF_calc(rcp26, 1900, 2000, "RCP 2.6")
+td <- tracking_results(rcp26, 1800, 2050, "RCP 2.6")
 
+AF_2000 <- AF_calc(td, 1900, 2000)
+AF_2020 <- AF_calc(td, 1900, 2020)
+
+AF_values <- c()
+for(val in 1800:1950){
+  AF_values <- append(AF_values, AF_calc(td, val, 2000))
+}
+
+meanAF <- mean(AF_values)
+
+names <- c("KNORR 1","KNORR 2","KNORR 3","KNORR 4","KNORR 5","KNORR 6","KNORR 7","HECTOR")
+values <- c(0.45, 0.453, 0.518, 0.468, 0.468, 0.514, 0.449, meanAF)
+error <- c(0.022, 0.014, 0.064, 0.047, 0.051, 0.035, 0.014, 0)
+df <- data.frame(names, values, error)
+
+graph <- ggplot(df) +
+  aes(x=names, y=values) + 
+  geom_bar(stat="identity") +
+  geom_errorbar(aes(ymin=values-error, ymax=values+error), width=.2)+
+  ggtitle("Airborne Fraction Comparison")+
+  ylab("Airborne Fraction in 2020")+
+  xlab(NULL)
+
+graph
